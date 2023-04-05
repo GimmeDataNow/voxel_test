0ecc5eab3cc7f4d8387ed8a0a6cbb97ad47c1e5c
voxel_rust <alan.omazic@gmail.com>
2023-04-05 06:06:49 +0200

    added documentation

--- src/chunks/rendering/rendering_const.rs
+++ src/chunks/rendering/rendering_const.rs
@@ -16,11 +16,17 @@ pub const VERTICES: [[[f32; 3]; 4]; 6] = [
 pub const TRIANGLES: [u32; 6] = [0, 1, 2, 2, 1, 3];
 
 pub const NORMALS: [[f32; 3]; 6] = [
+    // Top
     [0.0, 1.0, 0.0],
+    // Front
     [0.0, 0.0, -1.0],
+    // Left
     [-1.0, 0.0, 0.0],
+    // Right
     [1.0, 0.0, 0.0],
+    // Back
     [0.0, 0.0, 1.0],
+    // Bottom
     [0.0, -1.0, 0.0]
 ];
 
